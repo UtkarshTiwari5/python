2d9a1056c5cd12d1ceb5d7a7ae1fb76d05496fd7
UtkarshTiwari5 <utkarshtiwari1772@gmail.com>
2024-12-26 01:18:49 +0530

    Add files via upload

---
+++ tut-1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter the number :";
+    cin>>a;
+    cout<<"Enter the number :";
+    cin>>b;
+    sum = a +b;
+    
+    cout<< " the sum is " << sum ;
+
+    
+
+}
